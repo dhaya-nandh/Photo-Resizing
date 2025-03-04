cf147f2f33c036c0b46966ce96d8d8282e2dd514
Dhayanandh <159157709+dhaya-nandh@users.noreply.github.com>
2025-03-04 20:05:26 +0530

    just try

---
+++ Show Photo/script.js
@@ -0,0 +1,30 @@
+var img=document.querySelector(".images")
+var v=document.getElementById("vi")
+var a=document.getElementById("aj")
+var k=document.getElementById("ka")
+var r=document.getElementById("ra")
+function vij(){
+     v.style.display="block"
+     a.style.display="none"   
+     k.style.display="none"
+     r.style.display="none"
+}
+function aji(){
+    a.style.display="block"
+    v.style.display="none"  
+    k.style.display="none"
+    r.style.display="none"
+}
+function kam(){
+    k.style.display="block"
+    v.style.display="none"
+    a.style.display="none"   
+    r.style.display="none"
+}
+function raj(){
+    r.style.display="block"
+    v.style.display="none"
+    a.style.display="none"   
+    k.style.display="none"
+   
+}
